09e052a4bd790dde60830df4d34e6a37acb1504b
Angel Barbosa <angelbarbosa211@gmail.com>
2023-06-27 14:23:24 -0500

    adding cards section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Head from "next/head"
 import { BsFillMoonStarsFill } from "react-icons/bs"
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
 import Image from "next/image"
+import design from "../public/design.png"
+import code from "../public/code.png"
+import consulting from "../public/consulting.png"
 
 export default function Home() {
   return (
@@ -48,8 +51,64 @@ export default function Home() {
           </div>
           <div className=""></div>
         </section>
-
-        <section></section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">insert text here later</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
+              necessitatibus. Inventore voluptatem impedit officiis alias nam,
+              saepe rem magni <span className="text-teal-500">word</span>{" "}
+              lksdlkdjfkldjfkld <span className="text-teal-500">word</span>{" "}
+              jsndj
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui,
+              necessitatibus. Inventore voluptatem impedit officiis alias nam,
+              saepe rem magn
+            </p>
+          </div>
+          <div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image alt="" src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">nice designs</h3>
+              <p className="py-2">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                obcaecati
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image alt="" src={code} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">nice designs</h3>
+              <p className="py-2">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                obcaecati
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image alt="" src={consulting} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">nice designs</h3>
+              <p className="py-2">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                obcaecati
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div></div>
+        </section>
       </main>
     </div>
   )
